390b4162b6d7ec5f1c56cca6a14b733dd695f40c
Huỳnh Ngọc Thành <74103606+Huynhngocthanh@users.noreply.github.com>
2021-01-05 14:00:28 +0700

    Add files via upload

---
+++ C2_BT.py
@@ -0,0 +1,44 @@
+#BT1,2
+x= int(input ("Nhập x ="))
+y= int(input ("Nhập y ="))
+tong= x+y
+tich= x*y
+hieu= x-y
+du= x%y
+nguyen= x//y
+print ("Tổng 2 số là",tong)
+print ("Tích 2 số là",tich)
+print ("Hiệu 2 số là",hieu)
+print ("Chia lấy phần dư =",du)
+print ("Chia lấy phần nguyên =",nguyen)
+#BT3
+a= float(input("a="))
+b= float(input("b="))
+print("Tổng là",a+b)
+print("Tích là",a*b)
+print("Hiệu là",a-b)
+print("Thương là",a/b)
+#BT4
+x = input(" Nhập x =")
+x= float(x)
+print("Trị tuyệt đối của x là", abs(x))
+print("Phần nguyên của x là",int(x))
+print("Chuyển thành số thực x =",float(x))
+y =input("Nhập y =")
+y= float(y)
+print("Lũy thừa x mũ y là",pow(x,y) )
+from math import sqrt
+print ("Căn bậc 2 của x là",sqrt(x/((x-1))))
+#BT5
+x = input(" Nhập x =")
+x= float(x)
+print("Trị tuyệt đối của x là", abs(x))
+print("Phần nguyên của x là",int(x))
+print("Chuyển thành số thực x =",float(x))
+y =input("Nhập y =")
+y= float(y)
+print("Lũy thừa x mũ y là",pow(x,y) )
+from math import sqrt
+print ("Căn bậc 2 của x là",sqrt(x/((x-1))))
+#BT6
+print ('"Sóng bắt đầu từ gió \nGió bắt đầu từ đâu \nEm cũng không biết nữa \nKhi nào ta yêu nhau" \n \t (Trích Sóng - Xuân Quỳnh)')
