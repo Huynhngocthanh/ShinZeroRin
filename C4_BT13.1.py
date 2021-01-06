d6ad4d048f3cdf782fb370cc3c735c17442431b9
Huỳnh Ngọc Thành <74103606+Huynhngocthanh@users.noreply.github.com>
2021-01-06 15:16:02 +0700

    Update C4_BT13.1.py

--- C4_BT13.1.py
+++ C4_BT13.1.py
@@ -1,20 +1,13 @@
-import os
-path = "C:\\"
-files = list()
-for r, d,f  in os.walk(path):
-    for file in d:
-        files.append(os.path.join(r, file))
-for f in files:
-    print(f)
-print("ket thuc bai")
-path = "C:\\"
-list1 = list()
-list2 = list()
-for r, d,f  in os.walk(path):
-    for file in f:
-        list1.append(file)
-    for file in d:
-        list2.append(file)
-print("danh sach tap tin:",list1)
-print("danh sach thu muc:",list2)
-print("ket thuc bai ")
+import os, glob
+print(dir(os)) # Các hàm có trong thư viện OS
+# Liệt kê các thư mục con trong ổ đĩa
+list1 = []
+list2 = []
+path = 'C:\\Users\\MyPC\\Documents'
+for root, dirs, files in os.walk(path):
+    ten_tap_tin = glob.glob(path)
+    list1.append(ten_tap_tin)
+    ten_thu_muc = os.path.dirname(path)
+    list2.append(ten_thu_muc)
+print('Tên tập tin là: ', list1)
+print('Tên thư mục là: ', list2)
